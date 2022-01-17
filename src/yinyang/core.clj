beb0cc5daf9eddb4fd73ccf4d4bfa54cf6a2a4ec
sto <son.c.to@gmail.com>
2022-01-17 16:13:50 +0700

    eval vectors and maps

--- src/yinyang/core.clj
+++ src/yinyang/core.clj
@@ -6,7 +6,7 @@
 (declare eval2)
 
 (defmacro apply2 [s-ex env]
-  `(let [foo# (prn {:apply2-s-ex ~s-ex})
+  `(let [foo# (log/info {:apply2-s-ex ~s-ex})
          s-ex# (map #(eval2 % ~env) ~s-ex)
          f# (first s-ex#)
          args# (rest s-ex#)
@@ -33,17 +33,24 @@
        (not (-> s-ex first symbol?)))))
 
 (defn eval2 [s-ex env]
-  (prn {:eval2-s-ex s-ex})
+  (log/info {:eval2-s-ex s-ex})
   (cond
     (symbol? s-ex)              (env s-ex)
+    (vector? s-ex)              (mapv #(eval2 % env) s-ex)
+    (map? s-ex)                 (into {} (for [[k v] s-ex]
+                                           [(eval2 k env)
+                                            (eval2 v env)]))
     (and (list? s-ex)
          (let [f (first s-ex)]
            (= f 'lambda)))      (let [params (second s-ex)
                                       body (drop 2 s-ex)
-                                      body (conj body 'do)]
-                                  (prn {:params params
-                                        :body body})
+                                      ;;body (conj body 'do)
+                                      ]
+                                  (log/info {:params params
+                                             :body body})
                                   (fn [args]
+                                    (log/info {:args args
+                                               :body body})
                                     (eval2 body (fn [y]
                                                   (if (= params y)
                                                     args
@@ -86,6 +93,16 @@
   (eval2 '(* x x) {'x 2
                    '* *})
 
+  (eval2 '((prn {:x1 x}) (* x x)) {'x 2
+                                   'prn prn})
+
+  (eval2 '(prn {:x1 x}) {'x 2
+                         'prn prn})
+  (eval2 '{:x x} {'x 1})
+  (eval2 '[x] {'x 2})
+
+  (list? [1])
+  
   ({'x 2} 'x)
   
   (apply2 '(inc 2))
