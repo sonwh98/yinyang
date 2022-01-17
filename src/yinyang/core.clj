1bbc79cc2f370ff7017e8f5a4e3525509e5fa5df
sto <son.c.to@gmail.com>
2022-01-17 15:15:18 +0700

    timbre logging

--- src/yinyang/core.clj
+++ src/yinyang/core.clj
@@ -1,12 +1,13 @@
 (ns yinyang.core
-  (:require [clojure.pprint :as pp])
+  (:require [clojure.pprint :as pp]
+            [taoensso.timbre :as log])
   (:gen-class))
 
 (declare eval2)
 
 (defmacro apply2 [s-ex env]
-  (prn {:s-ex s-ex})
-  `(let [s-ex# (map #(eval2 % ~env) ~s-ex)
+  `(let [foo# (prn {:apply2-s-ex ~s-ex})
+         s-ex# (map #(eval2 % ~env) ~s-ex)
          f# (first s-ex#)
          args# (rest s-ex#)
          args-count# (count args#)]
@@ -32,29 +33,37 @@
        (not (-> s-ex first symbol?)))))
 
 (defn eval2 [s-ex env]
-
+  (prn {:eval2-s-ex s-ex})
   (cond
     (symbol? s-ex)              (env s-ex)
     (and (list? s-ex)
          (let [f (first s-ex)]
-           (= f 'lambda)))      (let [[x] (second s-ex)
-                                      body (last s-ex)]
-                                  (prn {
+           (= f 'lambda)))      (let [params (second s-ex)
+                                      body (drop 2 s-ex)
+                                      body (conj body 'do)]
+                                  (prn {:params params
                                         :body body})
                                   (fn [args]
-                                    
                                     (eval2 body (fn [y]
-                                                  (if (= x y)
+                                                  (if (= params y)
                                                     args
                                                     (env y))))))    
-    (list? s-ex) (do
-                   (prn {:s-ex s-ex})
-                   (apply2 s-ex env))
-    :else (do
-            (prn {:bar s-ex})
-            s-ex)))
+    (list? s-ex) (apply2 s-ex env)
+    :else s-ex))
 
 (comment
+  (require '[taoensso.timbre.appenders.core :as appenders])
+  (log/merge-config! {:min-level :info
+                      :middleware [(fn [data]
+                                     (update data :vargs (partial mapv #(if (string? %)
+                                                                          %
+                                                                          (with-out-str (pp/pprint %))))))]
+                      :appenders {:println {:enabled? false}
+                                  :catalog (merge (appenders/spit-appender {:fname (let [log-dir (or (System/getenv "LOG_DIR") ".")]
+                                                                                     (str  log-dir "/debug.log"))})
+                                                  {:min-level :info
+                                                   :level :info})}})
+  
   (self-eval? '(1 2 3))
   (self-eval? '(+ 2 (+ 1 1 1)))
   
@@ -68,9 +77,11 @@
 
   (eval2 '(lambda [x] (* x x)) {})
   (eval2 '((lambda [x]
+                   (prn {:x x})
                    (* x x)
                    ) 2) {'x 2
-                         '* *})
+                         '* *
+                         'prn prn})
 
   (eval2 '(* x x) {'x 2
                    '* *})
@@ -78,4 +89,10 @@
   ({'x 2} 'x)
   
   (apply2 '(inc 2))
+  (take-last 2 [1 2 3 4 5])
+  (conj 1 '(2 3))
+  (conj '(1) 2)
+
+
+
   )
