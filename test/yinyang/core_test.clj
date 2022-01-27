1fbfae1cbae32d5d4b605d96ba105480a6bec4a4
sto <son.c.to@gmail.com>
2022-01-27 16:04:42 +0000

    unit tests

--- test/yinyang/core_test.clj
+++ test/yinyang/core_test.clj
@@ -3,5 +3,19 @@
             [yinyang.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "eval data structures"
+    (is (= '(1 2 3)
+           (eval2 ''(1 2 3) {})))
+    (is (= [1 2 3]
+           (eval2 '[1 2 3] {})))
+    (is (= {:name "foo" :age 1}
+           (eval2 '{:name "foo" :age 1} {}))))
+
+  (testing "arithmatic expressions"
+    (is (= 5 (eval2 '(+ 2  3)
+                    {'+ +})))
+    (is (= 8 (eval2 '(+ 2 (* 2 3))
+                    {'+ +
+                     '* *}))))
+  
+  )
