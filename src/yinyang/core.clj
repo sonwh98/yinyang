3885383c3b542205d13c2622d0120ead675ee366
sto <son.c.to@gmail.com>
2022-02-03 11:49:59 +0000

    rearrange cond predicates and format code

--- src/yinyang/core.clj
+++ src/yinyang/core.clj
@@ -39,54 +39,56 @@
               :env env
               :global-env @global-env})
   (cond
-    (set? s-ex)    (set (map #(eval2 % env) s-ex))
-    (vector? s-ex) (mapv #(eval2 % env) s-ex)
-    (map? s-ex)    (into {} (for [[k v] s-ex]
-                              [(eval2 k env)
-                               (eval2 v env)]))
-    (p/quote? s-ex) (first (rest s-ex))
-    (p/do? s-ex)    (let [do-body (rest s-ex)
-                          last-ex (last do-body)
-                          ex-but-last (drop-last do-body)]
-                      (map #(eval2 % env) ex-but-last)
-                      (log/debug {:do-body do-body
-                                  :ex-but-last ex-but-last
-                                  :last-ex last-ex})
-                      (eval2 last-ex env))
-    (p/let? s-ex)     (let [bindings (second s-ex)
-                            pairs (mapv vec (partition 2 bindings))
-                            env2 (into {} pairs)
-                            body (drop 2 s-ex)
-                            implicit-do (conj body 'do)]
-                        (log/debug {:bindings bindings
-                                    :do implicit-do})
-                        (eval2 implicit-do (merge env env2)))
-    (symbol? s-ex)    (let [v (or (env s-ex)
-                                  (@global-env s-ex))]
-                        (log/debug {:s s-ex
-                                    :v v})
-                        v)
-    (p/def? s-ex)      (let [[d s v] s-ex]
-                         (swap! global-env (fn [global-env]
-                                             (assoc global-env s (eval2 v {}))))
-                         v)
-    (p/lambda? s-ex)  (let [params (second s-ex)
-                            body (drop 2 s-ex)
-                            body (conj body 'do)]
-                        (fn [& args]
-                          (eval2 body (fn [a-symbol]
-                                        (let [v (env a-symbol)]
-                                          (log/debug {:params params
-                                                      :a-symbol a-symbol
-                                                      :a-symbol-v v
-                                                      :args args})
-                                          (if v
-                                            v
-                                            (let [pairs (mapv vec (partition 2 (interleave params args)))
-                                                  env2 (into {} pairs)]
-                                              (env2 a-symbol))))))))
-    (seq? s-ex) (apply2 s-ex env)
-    :else s-ex))
+    (p/lambda? s-ex) (let [params (second s-ex)
+                           body (drop 2 s-ex)
+                           body (conj body 'do)]
+                       (fn [& args]
+                         (eval2 body (fn [a-symbol]
+                                       (let [v (env a-symbol)]
+                                         (log/debug {:params params
+                                                     :a-symbol a-symbol
+                                                     :a-symbol-v v
+                                                     :args args})
+                                         (if v
+                                           v
+                                           (let [pairs (mapv vec (partition 2 (interleave params args)))
+                                                 env2 (into {} pairs)]
+                                             (env2 a-symbol))))))))
+    
+    (set? s-ex)      (set (map #(eval2 % env) s-ex))
+    (vector? s-ex)   (mapv #(eval2 % env) s-ex)
+    (map? s-ex)      (into {} (for [[k v] s-ex]
+                                [(eval2 k env)
+                                 (eval2 v env)]))
+    (p/quote? s-ex)  (first (rest s-ex))
+    (p/do? s-ex)     (let [do-body (rest s-ex)
+                           last-ex (last do-body)
+                           ex-but-last (drop-last do-body)]
+                       (map #(eval2 % env) ex-but-last)
+                       (log/debug {:do-body do-body
+                                   :ex-but-last ex-but-last
+                                   :last-ex last-ex})
+                       (eval2 last-ex env))
+    (p/let? s-ex)    (let [bindings (second s-ex)
+                           pairs (mapv vec (partition 2 bindings))
+                           env2 (into {} pairs)
+                           body (drop 2 s-ex)
+                           implicit-do (conj body 'do)]
+                       (log/debug {:bindings bindings
+                                   :do implicit-do})
+                       (eval2 implicit-do (merge env env2)))
+    (symbol? s-ex)   (let [v (or (env s-ex)
+                                 (@global-env s-ex))]
+                       (log/debug {:s s-ex
+                                   :v v})
+                       v)
+    (p/def? s-ex)    (let [[d s v] s-ex]
+                       (swap! global-env (fn [global-env]
+                                           (assoc global-env s (eval2 v {}))))
+                       v)
+    
+    (seq? s-ex)      (apply2 s-ex env)
+    :else            s-ex))
 
 (defn file->forms
   "parse file into clojure s-expression forms"
