1ec1b54a71d30a689cdde2271b2eaf558878a6dc
Sonny <son.c.to@gmail.com>
2025-03-23 19:40:23 +0700

    Eval multiple top level forms (#10)
    * Move REPL code from clj.rs to repl.rs as its own module
    * merge start_repl into repl
    * Refactor slurp to split slurp into two function slurp_wrapper
    * allow read/eval of multiple top level forms for scripting to work

--- src/bin/m.rs
+++ src/bin/m.rs
@@ -1,14 +1,8 @@
-use bigdecimal::BigDecimal;
-use num_bigint::BigInt;
-use std::collections::HashMap;
-use std::str::FromStr;
-use yinyang::clojure::{eval, read_string, Value};
-use yinyang::core::register_native_fn;
-use yinyang::edn::*;
-use yinyang::immutant::list::*;
+use yinyang::clojure::read_string;
+use yinyang::core::*;
 
 fn main() {
-    let s = "[1\n2\n3 4]";
-    let v: Vec<&str> = s.split_whitespace().collect();
-    println!("{:?}", v);
+    let s = slurp("add.clj").unwrap();
+    let ast = read_string(&s).unwrap();
+    println!("{:?}", ast);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod clojure;
 pub mod core;
 pub mod edn;
 pub mod immutant;
+pub mod repl;
